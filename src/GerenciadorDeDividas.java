dfa674ad002ce4ae9f0dd85c137f0f1f85025509
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-13 15:21:54 -0300

    Aula 01 - Revisitando a Orientação a Objetos

---
+++ src/GerenciadorDeDividas.java
@@ -0,0 +1,11 @@
+public class GerenciadorDeDividas {
+
+    public void efetuaPagamento(Divida divida, double valor, String nomePagador, String cnpjPagador) {
+        Pagamento pagamento = new Pagamento();
+        pagamento.setCnpjPagador(cnpjPagador);
+        pagamento.setPagador(nomePagador);
+        pagamento.setValor(valor);
+        divida.paga(valor);
+        divida.registra(pagamento);
+    }
+}
